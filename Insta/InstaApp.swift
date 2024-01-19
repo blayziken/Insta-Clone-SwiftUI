fc338fcc329027232f65521153554a825165295b
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2024-01-18 23:15:57 -0500

    Initial Commit

---
+++ Insta/InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaApp.swift
+//  Insta
+//
+//  Created by Blaze on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
